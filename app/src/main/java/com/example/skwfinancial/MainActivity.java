a18ee00a55f69f95a34400c459c6b83c4edbae80
oldslann <592568928@qq.com>
2020-03-02 19:44:30 +0800

    login

--- app/src/main/java/com/example/skwfinancial/MainActivity.java
+++ app/src/main/java/com/example/skwfinancial/MainActivity.java
@@ -2,17 +2,41 @@ package com.example.skwfinancial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    private  static String TAG="main";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            Class.forName("android.os.AsyncTask");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         Button btn1=findViewById(R.id.loginbtn);
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,9 +58,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void int1()
     {
-        Intent intent=new Intent(this,tableact.class);
-        startActivity(intent);
-        this.finish();
+
+
+        String url = "http://192.168.1.102:8080/Mserver/home/FirstS";//http://192.168.1.102:8080/home/FirstS
+        new LoginTask().execute(url);
 
 
     }
@@ -47,4 +72,93 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
+    class  LoginTask extends AsyncTask<String,Integer,String> {
+        @Override
+        protected String doInBackground(String... params) {
+            String par  = params[0];
+            URL url = null;
+            HttpURLConnection connection = null;
+            String result="";
+            try {
+                url = new URL(par);
+                connection = (HttpURLConnection) url.openConnection(); // 打开该URL连接
+                Log.i(TAG, "ping" );
+
+                // 第二步：设置HttpURLConnection连接相关属性
+                connection.setRequestMethod("POST"); // 设置请求方法，“POST或GET”，我们这里用GET，在说到POST的时候再用POST
+                connection.setConnectTimeout(80000); // 设置连接建立的超时时间
+                connection.setReadTimeout(80000); // 设置网络报文收发超时时间
+                // 如果是POST方法，需要在第3步获取输入流之前向连接写入POST参数
+                DataOutputStream out = new DataOutputStream(connection.getOutputStream()); // 在此之前也可以用connection.getResponseCode()获取返回码，看是否为200
+                EditText t1=(EditText)findViewById(R.id.usrname);
+                EditText t2=(EditText)findViewById(R.id.userpassword);
+
+
+                @SuppressLint("WrongThread") String id=t1.getText().toString().trim();
+                @SuppressLint("WrongThread") String userp=t2.getText().toString().trim();
+
+                String param = "id="+id+"&password="+userp; // 这里我们先模拟一个参数列表
+                out.writeBytes(param);
+
+                Log.i(TAG, "out" );
+
+                // 第三步：打开连接输入流读取返回报文，*注意*在此步骤才真正开始网络请求
+                InputStream in = connection.getInputStream();  // 通过连接的输入流获取下发报文，然后就是Java的流处理
+
+                Log.i(TAG, "in" );
+                // ...省略返回报文处理代码
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                String line;
+                StringBuilder response = new StringBuilder();
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+
+                result=response.toString();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Log.i(TAG, result );
+            return result;
+
+        }
+        //第四步
+        @Override
+        protected void onPostExecute(String result) {//weizhixing
+            //super.onPostExecute(result);
+            //保存数据
+            if(result.equals("1")){
+                Toast.makeText(getApplicationContext(),"登陆成功",Toast.LENGTH_SHORT).show();
+                Log.i(TAG, "rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr" );
+
+                Intent intent=new Intent(MainActivity.this,tableact.class);
+                startActivity(intent);
+                MainActivity.this.finish();
+                return;
+            }
+            else{
+
+                Toast.makeText(getApplicationContext(),"登陆失败,账号或密码有误",Toast.LENGTH_SHORT).show();
+                Log.i(TAG, "rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr" );
+                return;
+
+            }
+
+        }
+    }
+
+
+
+
+
+
+
 }
--- app/src/main/java/com/example/skwfinancial/signact.java
+++ app/src/main/java/com/example/skwfinancial/signact.java
@@ -2,14 +2,28 @@ package com.example.skwfinancial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class signact extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 
+public class signact extends AppCompatActivity {
+    private  static String TAG="main";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,8 +58,9 @@ public class signact extends AppCompatActivity {
             if (sa.equals(sb))//两次密码相同
             {
 
-                Toast.makeText(signact.this,"注册成功",Toast.LENGTH_SHORT).show();
-                this.finish();
+                String url = "http://192.168.1.102:8080/Mserver/home/NewUser";
+                new NewTask().execute(url);
+
 
             }
 
@@ -62,6 +77,87 @@ public class signact extends AppCompatActivity {
 
 
 
+    }
+
+
+
+
+    class  NewTask extends AsyncTask<String,Integer,String> {
+        @Override
+        protected String doInBackground(String... params) {
+            String par  = params[0];
+            URL url = null;
+            HttpURLConnection connection = null;
+            String result="";
+            try {
+                url = new URL(par);
+                connection = (HttpURLConnection) url.openConnection(); // 打开该URL连接
+                Log.i(TAG, "ping" );
+
+                // 第二步：设置HttpURLConnection连接相关属性
+                connection.setRequestMethod("POST"); // 设置请求方法，“POST或GET”，我们这里用GET，在说到POST的时候再用POST
+                connection.setConnectTimeout(80000); // 设置连接建立的超时时间
+                connection.setReadTimeout(80000); // 设置网络报文收发超时时间
+                // 如果是POST方法，需要在第3步获取输入流之前向连接写入POST参数
+                DataOutputStream out = new DataOutputStream(connection.getOutputStream()); // 在此之前也可以用connection.getResponseCode()获取返回码，看是否为200
+                EditText t1=(EditText)findViewById(R.id.inusrname);
+                EditText t2=(EditText)findViewById(R.id.inp1);
+
+
+                @SuppressLint("WrongThread") String id=t1.getText().toString().trim();
+                @SuppressLint("WrongThread") String userp=t2.getText().toString().trim();
+
+                String param = "id="+id+"&password="+userp; // 这里我们先模拟一个参数列表
+                out.writeBytes(param);
+
+                Log.i(TAG, "out" );
+
+                // 第三步：打开连接输入流读取返回报文，*注意*在此步骤才真正开始网络请求
+                InputStream in = connection.getInputStream();  // 通过连接的输入流获取下发报文，然后就是Java的流处理
+
+                Log.i(TAG, "in" );
+                // ...省略返回报文处理代码
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                String line;
+                StringBuilder response = new StringBuilder();
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+
+                result=response.toString();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Log.i(TAG, result );
+            return result;
+
+        }
+        //第四步
+        @Override
+        protected void onPostExecute(String result) {//weizhixing
+            //super.onPostExecute(result);
+            //保存数据
+            if(result.equals("1")){
+                Toast.makeText(getApplicationContext(),"注册成功",Toast.LENGTH_SHORT).show();
+
+                signact.this.finish();
+                return;
+            }
+            else{
+
+                Toast.makeText(getApplicationContext(),"注册失败,该账号已存在",Toast.LENGTH_SHORT).show();
+
+                return;
+
+            }
+
+        }
     }
 
 
